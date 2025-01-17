e5382701ae29fab3185dac29ddc5abecd5e4148e
JaneCjr <jane_cjr@163.com>
2025-01-17 17:23:27 +0800

    feate:first commit global command "publish"

---
+++ bin/index.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+// 告诉操作系统用 Node 来运行此文件
+// 在本项目的根目录里执行 npm link ，将./bin下面的myGlobal命令挂载到全局
+// npm install commander
+const program = require("commander");
+
+const myhelp = require("../lib/core/help");
+const myCommander = require("../lib/core/commander");
+
+myhelp(program);
+myCommander.replaceByEnv(program);
+myCommander.replaceByProject(program);
+myCommander.handleConfig(program);
+myCommander.handleSearchConfig(program);
+myCommander.handleSearchConfig(program);
+myCommander.handleDeleteConfig(program);
+
+program.parse(process.argv);
---
+++ config.js
@@ -0,0 +1,6 @@
+// const pathInfo = require("./pathInfo.json");
+const pathInfo = require("./test.json");
+module.exports = {
+  //路径信息
+  pathInfo: pathInfo,
+};
